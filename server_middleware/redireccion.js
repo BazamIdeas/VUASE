58dd18b1aabf20d30701b17bdcc995da25dfa923
Cis <kalinca15@gmail.com>
2018-09-24 08:55:13 -0300

    ajustes

--- server_middleware/redireccion.js
+++ server_middleware/redireccion.js
@@ -2,11 +2,7 @@ const url = require('url')
 
 module.exports = function (req, res, next) {
   if (process.env.NODE_ENV !== 'production') {
-<<<<<<< HEAD
-    console.log('Estas en: ' + req.iso)
-=======
-    console.log("Estas en: "+ req.iso + ' (server)')
->>>>>>> bc1574175d1dd710b7168fa92999b79698b68d98
+    console.log('Estas en: ' + req.iso + ' (server)')
     next()
   } else {
     let iso = req.iso
@@ -14,14 +10,6 @@ module.exports = function (req, res, next) {
     let dominio = req.headers.host
 
     let path = url.parse(req.url).pathname
-<<<<<<< HEAD
-=======
-      
-    let paises =  {
-      "AR" : "liderlogo.com",
-      "ES" : "liderlogo.es"
-    }
->>>>>>> bc1574175d1dd710b7168fa92999b79698b68d98
 
     let paises = {
       'AR': 'liderlogo.com',
