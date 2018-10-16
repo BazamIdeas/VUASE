0f300381be73fc5e5a0b2437fe3bc363cb01b509
Cis <kalinca15@gmail.com>
2018-10-16 11:01:09 -0300

    liderlogos

--- server_middleware/redireccion.js
+++ server_middleware/redireccion.js
@@ -23,7 +23,7 @@ module.exports = function (req, res, next) {
         return res.end()
       }
     } else {
-      res.writeHead(301, {Location: 'https://www.liderlogo.com' + path})
+      res.writeHead(301, {Location: 'http://www.liderlogos.com' + path})
       return res.end()
     }
   }
