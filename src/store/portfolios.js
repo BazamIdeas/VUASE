afb1165f5133e24731a5628035aab68384c29103
LDTorres <luisdtc2696@gmail.com>
2019-01-10 12:04:38 -0400

    Portfolios

--- src/store/portfolios.js
+++ src/store/portfolios.js
@@ -98,49 +98,53 @@ export const mutations = {
 
 export const getters = {}
 
+var oldParams = {}
+
 export const actions = {
   async getAll ({ rootGetters, commit, state }, params) {
     if (pending) return // console.log('pending')
     pending = true
 
-    let requestParams = {}
-    if (params) {
-      if (
-        params.servicio &&
-        rootGetters['services/getBySlug'](params.servicio)
-      ) {
-        requestParams['services'] = rootGetters['services/getBySlug'](
-          params.servicio
-        ).id
-      }
-      if (params.sector && rootGetters['sectors/getBySlug'](params.sector)) {
-        requestParams['sectors'] = rootGetters['sectors/getBySlug'](
-          params.sector
-        ).id
-      }
-      if (
-        params.actividad &&
-        rootGetters['countries/getBySlug'](params.actividad)
-      ) {
-        requestParams['countries'] = rootGetters['countries/getBySlug'](params.actividad).id
-      }
-      if (params.pais && rootGetters['countries/getBySlug'](params.pais)) {
-        requestParams['countries'] = rootGetters['countries/getBySlug'](
-          params.pais
-        ).id
-      }
-      if (
-        params.localidad &&
-        rootGetters['countries/locations/getBySlug'](params.localidad)
-      ) {
-        requestParams['locations'] = rootGetters['countries/locations/getBySlug'](params.localidad).id
-      }
-    }
-
     params.offset = state.list.length
 
-    if (requestParams !== {} && params.offset !== 0) {
-      params.offset = 0
+    let requestParams = {}
+
+    if (params) {
+      let resetOffset = false
+
+      if (params.servicio && rootGetters['services/getBySlug'](params.servicio)) {
+        let el = rootGetters['services/getBySlug'](params.servicio)
+        if (oldParams['services'] !== el.id) resetOffset = true
+        requestParams['services'] = el.id
+      }
+
+      if (params.sector && rootGetters['sectors/getBySlug'](params.sector)) {
+        let el = rootGetters['sectors/getBySlug'](params.sector)
+        if (oldParams['sectors'] !== el.id) resetOffset = true
+        requestParams['sectors'] = el.id
+      }
+
+      if (params.actividad && rootGetters['countries/getBySlug'](params.actividad)) {
+        let el = rootGetters['countries/getBySlug'](params.actividad)
+        if (oldParams['countries'] !== el.id) resetOffset = true
+        requestParams['countries'] = el.id
+      }
+
+      if (params.pais && rootGetters['countries/getBySlug'](params.pais)) {
+        let el = rootGetters['countries/getBySlug'](params.pais)
+        if (oldParams['countries'] !== el.id) resetOffset = true
+        requestParams['countries'] = el.id
+      }
+
+      if (params.localidad && rootGetters['countries/locations/getBySlug'](params.localidad)) {
+        let el = rootGetters['countries/locations/getBySlug'](params.localidad)
+        if (oldParams['locations'] !== el.id) resetOffset = true
+        requestParams['locations'] = el.id
+      }
+
+      if (resetOffset && params.offset !== 0) {
+        params.offset = 0
+      }
     }
 
     let portfolios
@@ -151,6 +155,8 @@ export const actions = {
       portfolios = await this.$axios.$get(url, {
         params: requestParams
       })
+
+      oldParams = requestParams
 
       if (params.offset === 0) {
         return commit('GET_ALL', {portfolios: portfolios})
