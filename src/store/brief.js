7673dfdb44ffe042943f450fc8433bb067f14da8
Daniel Torres <danieljtorres94@gmail.com>
2019-07-05 15:25:38 -0300

    otra

--- src/store/brief.js
+++ src/store/brief.js
@@ -242,13 +242,12 @@ export const actions = {
     let location, saveBrief, token
 
     try {
-      console.log(this.$cookies.get('liderlogo_client_ip'))
-      location = await axios.get('http://ip-api.com/json/' + this.$cookies.get('liderlogo_client_ip'))
+      location = await axios.get('https://ipapi.co/' + this.$cookies.get('liderlogo_client_ip') + '/json/')
 
       saveBrief = await this.$axios.$post('briefs', bodyFormData, {
         headers: {
           'Content-Type': 'multipart/form-data',
-          'Location-Name': `${location.data.city}, ${location.data.regionName}, CP: ${location.data.zip}`
+          'Location-Name': `${location.data.city}, ${location.data.region}, CP: ${location.data.postal}`
         }
       })
       token = saveBrief.client.token
