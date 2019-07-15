6c11ba6fcc852ae0badbb4871c5293a7ea324bdc
Daniel Torres <danieljtorres94@gmail.com>
2019-07-15 08:24:55 -0300

    todo bien

--- src/pages/como-funciona.vue
+++ src/pages/como-funciona.vue
@@ -35,7 +35,7 @@
             <v-layout row wrap class="paso paso-1 mt-5" style="background-image: url(images/pages/paso-1.svg)">
                 <v-flex xs12 md6 class="text-xs-center py-5 ">
                   <h2 class="active title-step step-1 py-5" style="font-size: 50px; line-height: 45px; font-weight: 600;">Elabora <br class="hidden-sm-and-down">un brief</h2>
-                  <img class="my-5" width="80%" src="images/pages/como-hacer-un-logotipo.jpg" alt="">
+                  <img class="my-5" width="80%" src="images/pages/como-hacer-un-logotipo.png" alt="">
                 </v-flex>
                 <v-flex xs12 md6 class="px-3">
                     <!--<h2 style="font-size: 50px; line-height: 45px; font-weight: 600;">Elabora <br class="hidden-sm-and-down">un brief</h2>-->
