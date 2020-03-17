655dabbb2a828d4b915fc067088d66a72519b081
Cis <kalinca15@gmail.com>
2020-03-16 21:37:29 -0300

    aviso superior

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,11 @@
 <template>
+<div>
+  <div style="text-align: center; padding: 5px; background: #c7c5c1;">
+    En LiderLogo 
+    <span style="font-weight: 800;">no cerramos,</span> 
+    seguimos trabajando para ti y para que 
+    <span style="padding: 4px; background: #fdc779; font-weight: 800; font-style: oblique;">tú negocio crezca</span>
+  </div>
   <v-toolbar class="transparent" height="100" flat style="position:absolute; z-index: 4;">
     <v-spacer class="hidden-sm-and-down"></v-spacer>
     <v-toolbar-side-icon @click.stop="$store.dispatch('toggleDrawer')" class="hidden-md-and-up menu-burger"></v-toolbar-side-icon>
@@ -42,6 +49,7 @@
     </v-tooltip>
     <v-spacer class="hidden-sm-and-down"></v-spacer>
   </v-toolbar>
+</div>
 </template>
 
 <script>
